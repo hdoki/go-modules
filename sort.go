f7009dfcdc1f84fe202c6531e068fe6ab75cf429
Harini Doki <hdoki@hgsdigital.com>
2022-03-08 11:32:47 -0600

    this is my second commit

--- sort.go
+++ sort.go
@@ -8,12 +8,13 @@ type customer struct {
 }
 
 func main() {
-	var customers [5]customer
+	var customers [6]customer
 	customers[0] = customer{1, "a"}
 	customers[1] = customer{2, "b"}
 	customers[2] = customer{3, "c"}
 	customers[3] = customer{4, "d"}
 	customers[4] = customer{5, "e"}
+	customers[5] = customer{6, "f"}
 
 	fmt.Println(customers)
 }
